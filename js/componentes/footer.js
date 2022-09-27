f621bc3c77b487cf07c0a30e1386877d7eb89784
Steve Gomez Boror <stevegomitas3@gmail.com>
2022-09-27 11:38:16 -0600

    .

---
+++ js/componentes/footer.js
@@ -0,0 +1,60 @@
+export let insertar_footer = 
+    '<footer>' + 
+        '<div class="seccion_1_footer">' + 
+            '<img src="https://github.com/NottStevE/Imagenes/blob/main/Proyecto%20aps/logo%20footer.png?raw=true">' +
+            '<div>' + 
+                '<h2>' + 
+                    'Biólogas' +
+                '</h2>' +
+                '<p>' + 
+                    '-Madelyne Toc' +
+                '</p>' +
+                '<p>' + 
+                    '-Estefany Jimenez' +
+                '</p>' +
+                '<p>' + 
+                    '-Karen Dávila' +
+                '</p>' +
+                '<h2 class="espacio">' + 
+                    'Diseñadoras' +
+                '</h2>' +
+                '<p>' + 
+                    '-Karen Boror' +
+                '</p>' +
+                '<p>' + 
+                    '-Kristell García' +
+                '</p>' +
+            '</div>' +
+            '<div>' + 
+                '<h2>' + 
+                    'Programadores' +
+                '</h2>' +
+                '<p>' + 
+                    '-Brandon Gómez' +
+                '</p>' +
+                '<p>' + 
+                    '-Marlon Martínez' +
+                '</p>' +
+                '<p>' + 
+                    '-Alan Monasterio' +
+                '</p>' +
+
+                '<h2 class="espacio">' + 
+                    'Referencias' +
+                '</h2>' +
+                '<p>' + 
+                    '-TablaCAlimentos.pdf' +
+                '</p>' +
+                '<p>' + 
+                    '-Merline Plus.com' +
+                '</p>' +
+                '<p>' + 
+                    '-Aprende.com' +
+                '</p>' +
+            '</div>' +
+        '</div>' +
+        '<div class="seccion_2_footer">' + 
+            '<p>' + 'Santa Catalina Laboure ApS 2022' + '</p>' +
+        '</div>' +
+    '</footer>'
+;
