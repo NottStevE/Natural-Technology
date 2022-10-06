d62178df3f788b0830f9615a55a1a7674c6ad760
Alan Monasterio <aemonasterio@scl.edu.gt>
2022-10-05 20:01:08 -0600

    actualice la base de datos de los carbohidratos

--- js/componentes/DATA/carbohidratos_DATA.js
+++ js/componentes/DATA/carbohidratos_DATA.js
@@ -1,42 +1,83 @@
 export let carbohidratos_DATA = [
     {
         nombre: 'PERA',
-        alimentos_imagenes: 'https://github.com/NottStevE/Imagenes/blob/main/Proyecto%20aps/CARBOHIDRATOS/Pera%20(1).png?raw=true'
+        alimentos_imagenes: 'https://github.com/NottStevE/Imagenes/blob/main/Proyecto%20aps/CARBOHIDRATOS/Pera%20(1).png?raw=true',
+      kcla: "0.056",
+      cal: "56",
+      estadistica: "https://github.com/Alanestuardo/Img2/blob/main/Component%2099.png?raw=true",
+      porcentaje: "https://github.com/Alanestuardo/Img2/blob/main/Component%2013.png?raw=true"
     },
     {
         nombre: 'PIÑA',
-        alimentos_imagenes: 'https://github.com/NottStevE/Imagenes/blob/main/Proyecto%20aps/CARBOHIDRATOS/Pi%C3%B1a%20(1).png?raw=true'
+        alimentos_imagenes: 'https://github.com/NottStevE/Imagenes/blob/main/Proyecto%20aps/CARBOHIDRATOS/Pi%C3%B1a%20(1).png?raw=true',
+        kcla: "0.052",
+        cal: "52",
+        estadistica: "https://github.com/Alanestuardo/Img2/blob/main/Component%2098.png?raw=true",
+        porcentaje: "https://github.com/Alanestuardo/Img2/blob/main/Component%2012%20(2).png?raw=true"
     },
     {
         nombre: 'GARBANZO',
-        alimentos_imagenes: 'https://github.com/NottStevE/Imagenes/blob/main/Proyecto%20aps/CARBOHIDRATOS/Garbanzo.png?raw=true'
+        alimentos_imagenes: 'https://github.com/NottStevE/Imagenes/blob/main/Proyecto%20aps/CARBOHIDRATOS/Garbanzo.png?raw=true',
+        kcla: "0.338",
+        cla: "338",
+        estadistica: "https://github.com/Alanestuardo/Img2/blob/main/Component%20101.png?raw=true",
+        porcentaje: "https://github.com/Alanestuardo/Img2/blob/main/Component%2019%20(1).png?raw=true"
     },
     {
         nombre: 'FRESA',
-        alimentos_imagenes: 'https://github.com/NottStevE/Imagenes/blob/main/Proyecto%20aps/CARBOHIDRATOS/Fresa.png?raw=true'
+        alimentos_imagenes: 'https://github.com/NottStevE/Imagenes/blob/main/Proyecto%20aps/CARBOHIDRATOS/Fresa.png?raw=true',
+        kcla: "0.032",
+        cla: "32",
+        estadistica: "https://github.com/Alanestuardo/Img2/blob/main/Component%20102.png?raw=true",
+        porcentaje: "https://github.com/Alanestuardo/Img2/blob/main/Component%2020%20(1).png?raw=true"
+        
     },
     {
         nombre: 'UVA',
-        alimentos_imagenes: 'https://github.com/NottStevE/Imagenes/blob/main/Proyecto%20aps/CARBOHIDRATOS/Uva.png?raw=true'
+        alimentos_imagenes: 'https://github.com/NottStevE/Imagenes/blob/main/Proyecto%20aps/CARBOHIDRATOS/Uva.png?raw=true',
+        kcla: "0.067",
+        cla: "67",
+        estadistica: "https://github.com/Alanestuardo/Img2/blob/main/Component%20100.png?raw=true",
+        porcentaje: "https://github.com/Alanestuardo/Img2/blob/main/Component%2014.png?raw=true"
     },
     {
         nombre: 'KIWI',
-        alimentos_imagenes: 'https://github.com/NottStevE/Imagenes/blob/main/Proyecto%20aps/CARBOHIDRATOS/Kiwi.png?raw=true'
+        alimentos_imagenes: 'https://github.com/NottStevE/Imagenes/blob/main/Proyecto%20aps/CARBOHIDRATOS/Kiwi.png?raw=true',
+        kcla: "0.057",
+        cla: "57",
+        estadistica: "https://github.com/Alanestuardo/Img2/blob/main/Component%20103.png?raw=true",
+        porcentaje: "https://github.com/Alanestuardo/Img2/blob/main/Component%2021%20(1).png?raw=true"
     },
     {
         nombre: 'LIMÓN',
-        alimentos_imagenes: 'https://github.com/NottStevE/Imagenes/blob/main/Proyecto%20aps/CARBOHIDRATOS/Limon.png?raw=true'
+        alimentos_imagenes: 'https://github.com/NottStevE/Imagenes/blob/main/Proyecto%20aps/CARBOHIDRATOS/Limon.png?raw=true',
+        kcla: "0.029",
+        cla: "29",
+        estadistica: "https://github.com/Alanestuardo/Img2/blob/main/Component%20104.png?raw=true",
+        porcentaje: "https://github.com/Alanestuardo/Img2/blob/main/Component%2017.png?raw=true"
     },
     {
         nombre: 'MANGO',
-        alimentos_imagenes: 'https://github.com/NottStevE/Imagenes/blob/main/Proyecto%20aps/CARBOHIDRATOS/Mango.png?raw=true'
+        alimentos_imagenes: 'https://github.com/NottStevE/Imagenes/blob/main/Proyecto%20aps/CARBOHIDRATOS/Mango.png?raw=true',
+        kcla: "0.065",
+        cla: "208",
+        estadistica: "https://github.com/Alanestuardo/Img2/blob/main/Component%20105.png?raw=true",
+        porcentaje: "https://github.com/Alanestuardo/Img2/blob/main/Component%2025.png?raw=true"
     },
     {
         nombre: 'NARANJA',
-        alimentos_imagenes: 'https://github.com/NottStevE/Imagenes/blob/main/Proyecto%20aps/CARBOHIDRATOS/Naranja.png?raw=true' 
+        alimentos_imagenes: 'https://github.com/NottStevE/Imagenes/blob/main/Proyecto%20aps/CARBOHIDRATOS/Naranja.png?raw=true',
+        kcla: "0.045",
+        cla: "45",
+        estadistica: "https://github.com/Alanestuardo/Img2/blob/main/Component%20106.png?raw=true",
+        porcentaje: "https://github.com/Alanestuardo/Img2/blob/main/Component%2026.png?raw=true"
     },
     {
         nombre: 'BANANA',
-        alimentos_imagenes: 'https://github.com/NottStevE/Imagenes/blob/main/Proyecto%20aps/CARBOHIDRATOS/Banano.png?raw=true'
+        alimentos_imagenes: 'https://github.com/NottStevE/Imagenes/blob/main/Proyecto%20aps/CARBOHIDRATOS/Banano.png?raw=true',
+        kcla: "0.09",
+        cla: "90",
+        estadistica: "https://github.com/Alanestuardo/Img2/blob/main/Component%20107.png?raw=true",
+        porcentaje: "https://github.com/Alanestuardo/Img2/blob/main/Component%2027.png?raw=true"
     }
 ]
