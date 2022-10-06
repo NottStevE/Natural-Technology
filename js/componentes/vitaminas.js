2d16d1b820aff4705374a7a4d3e05648c2a98b68
Steve Gomez Boror <stevegomitas3@gmail.com>
2022-10-06 03:14:23 -0600

    Se termino la seccion vitaminas.

--- js/componentes/vitaminas.js
+++ js/componentes/vitaminas.js
@@ -1,4 +1,3 @@
-import { insertar_footer } from "./footer.js";
 import { vitaminas_DATA } from "./DATA/vitaminas_DATA.js";
 
 export let menu = 
@@ -34,6 +33,7 @@ export let menu =
                         '<div id="boton_vitaminas">' + 
                             '<a href="./vitaminas.html">' + 'Vitaminas' + '</a>' +
                         '</div>' +
+                        '<img src="https://github.com/NottStevE/Imagenes/blob/main/Proyecto%20aps/salida%201.png?raw=true" id="close">' +
                     '</div>' +
                 '</div>' +
             '</div>' +
@@ -42,21 +42,22 @@ export let menu =
         '</div>' +
     '</div>'
 ;
-export function pagina_lipidos(){
+export function pagina_proteina(){
     let contenedor_proteina = 
         '<div class="contenedor_proteina">'+
             '<div class="seccion_1">' +
                 '<div class="contenedor_video">' + 
-                '<img src="https://github.com/NottStevE/Imagenes/blob/main/Proyecto%20aps/personaje%20naranja.png?raw=true" class="personaje_naranja">' + 
-                '<div class="caja_video">' + 
-                    '<iframe width="560" height="315" src="https://www.youtube.com/embed/5CvWTEpEXY8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>' +
-                    '<div>' +
-                        '<p>' + 'Funciones de las Vitaminas en el Organismo' + '</p>' +
-                        '<h2>' + '2min' + '</h2>' +
+                    '<img src="https://github.com/NottStevE/Imagenes/blob/main/Proyecto%20aps/personaje%20naranja.png?raw=true" class="personaje_naranja">' + 
+                    '<div class="caja_video">' + 
+                        '<iframe width="560" height="315" src="https://www.youtube.com/embed/FWvrOMpb8IE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>' + '</iframe>' +
+                        '<div>' +
+                            '<p>' + 'Funciones de las Vitaminas en el Organismo' + '</p>' +
+                            '<h2>' + '3min' + '</h2>' +
+                        '</div>' +
                     '</div>' +
                 '</div>' +
-                '</div>' +
                 '<div class="caja_texto_proteina">' + 
+                    '<img src="https://github.com/NottStevE/Imagenes/blob/main/Proyecto%20aps/personaje%20naranja.png?raw=true" class="personaje_naranja_2">' + 
                     '<img src="https://github.com/NottStevE/Imagenes/blob/main/Proyecto%20aps/decoracion%20texto.png?raw=true" class="decoracion_texto_1">' +
                     '<p>' + 'Con la visualización del video ya tienes más conocimientos acerca de las vitaminas, que te parece si conocemos las propiedades de algunos de ellos!!' + '</p>' +
                     '<img src="https://github.com/NottStevE/Imagenes/blob/main/Proyecto%20aps/decoracion%20texto%202.png?raw=true" class="decoracion_texto_2">' +
@@ -67,33 +68,104 @@ export function pagina_lipidos(){
                 '<div class="titulos_proteina">' + 
                     '<h2 class="titulo_01">' + 'Conoce' + '</h2>' +
                     '<h2 class="titulo_02">' + 'Alimentos' +' </h2>' +
-                    '<h2 class="titulo_03">' + 'Ricos en' + '</h2>' +
-                    '<h2 class="titulo_04">' + 'Vitaminas' + '</h2>' +
+                    '<h2 class="titulo_03">' + 'Ricos' + '</h2>' +
+                    '<h2 class="titulo_04">' + 'En Vitaminas' + '</h2>' +
                 '</div>' +
                 '<div id="contenedor_alimentos">' + 
                 
                 '</div>' +
-                '<div>' + 
-                    '<img src="" class="titulo_propiedades">'
+            '</div>' +
+            '<div class="seccion_3">' + 
+                '<img src="https://github.com/NottStevE/Imagenes/blob/main/Proyecto%20aps/Component%20118.png?raw=true" class="titulo_propiedades">' +
+                '<div id="info_alimentos_contenedor">' + 
+                
                 '</div>' +
+                '<img src="https://github.com/NottStevE/Imagenes/blob/main/Proyecto%20aps/Component%20119.png?raw=true" class="titulo_recordatorio">' +
+                '<img src="https://github.com/NottStevE/Imagenes/blob/main/Proyecto%20aps/Component%20132.png?raw=true" class="recordatorio_movil">' + 
+                '<img src="https://github.com/NottStevE/Imagenes/blob/main/Proyecto%20aps/Component%20123.png?raw=true" class="recordatorio_compu">' +
+                '<footer>' + 
+        '<div class="seccion_1_footer">' + 
+            '<img src="https://github.com/NottStevE/Imagenes/blob/main/Proyecto%20aps/logo%20footer.png?raw=true">' +
+            '<div>' + 
+                '<h2>' + 
+                    'Biólogas' +
+                '</h2>' +
+                '<p>' + 
+                    '-Madelyne Toc' +
+                '</p>' +
+                '<p>' + 
+                    '-Estefany Jimenez' +
+                '</p>' +
+                '<p>' + 
+                    '-Karen Dávila' +
+                '</p>' +
+                '<h2 class="espacio">' + 
+                    'Diseñadoras' +
+                '</h2>' +
+                '<p>' + 
+                    '-Karen Boror' +
+                '</p>' +
+                '<p>' + 
+                    '-Kristell García' +
+                '</p>' +
+            '</div>' +
+            '<div>' + 
+                '<h2>' + 
+                    'Programadores' +
+                '</h2>' +
+                '<p>' + 
+                    '-Brandon Gómez' +
+                '</p>' +
+                '<p>' + 
+                    '-Marlon Martínez' +
+                '</p>' +
+                '<p>' + 
+                    '-Alan Monasterio' +
+                '</p>' +
+
+                '<h2 class="espacio">' + 
+                    'Referencias' +
+                '</h2>' +
+                '<p>' + 
+                    '-TablaCAlimentos.pdf' +
+                '</p>' +
+                '<p>' + 
+                    '-Merline Plus.com' +
+                '</p>' +
+                '<p>' + 
+                    '-Aprende.com' +
+                '</p>' +
+            '</div>' +
+        '</div>' +
+        '<div class="seccion_2_footer">' + 
+            '<p>' + 'Santa Catalina Laboure ApS 2022' + '</p>' +
+        '</div>' +
+    '</footer>'
             '</div>' +
         '</div>'
         ;
-    let caja_vitaminas = document.querySelector("#caja_vitaminas");
-    caja_vitaminas.innerHTML = menu + contenedor_proteina + insertar_footer;
+    let caja_proteina = document.querySelector("#caja_proteina");
 
+    caja_proteina.innerHTML = menu + contenedor_proteina;
+    //ABRIR MENU
     let desplegable = document.querySelector("#menu_desplegable");
     let boton_desplegable = document.querySelector("#boton_menu_desplegable");
     function menu_desplegable(){
         desplegable.classList.remove("desactivar");
     }
     boton_desplegable.addEventListener('click', menu_desplegable);
+    //CERRAR MENU
+    let close = document.querySelector("#close");
+    function menu_close(){
+        desplegable.classList.add("desactivar");
+    }
+    close.addEventListener('click', menu_close);
 
-    let alimentos_vitaminas = ['Huevos', 'Leche', 'Melón', 'Yogurt', 'Hígado', 'Espinaca', 'Zanahoria', 'Guisantes', 'Remolacha', 'Almendras'];
+    let alimentos_proteinas = ['Huevos', 'Leche', 'Melón', 'Yogurt', 'Hígado', 'Espinaca', 'Zanahoria', 'Guisantes', 'Remolacha', 'Almendras'];
     let contenedor_alimentos = document.querySelector("#contenedor_alimentos");
 
     function agregar_alimentos(){
-        alimentos_vitaminas.forEach(function(element, index){
+        alimentos_proteinas.forEach(function(element, index){
             let contenedor = document.createElement("div");
             contenedor.innerHTML = 
                 '<p>' + element + '</p>' +
@@ -103,5 +175,27 @@ export function pagina_lipidos(){
         });
     }
     agregar_alimentos();
+
+    let info_alimentos = document.querySelector("#info_alimentos_contenedor");
+    function agregar_info_alimentos(){
+        alimentos_proteinas.forEach(function(element, index){
+            let contenedor = document.createElement("div");
+            contenedor.innerHTML = 
+                '<h2 class="nombre_fruta_info">' + vitaminas_DATA[index].nombre + '</h2>' +
+                '<div class="info_cal">' + 
+                    '<p class="kcal">' + vitaminas_DATA[index].kcal + '%Kcal' + '</p>' +
+                    '<p class="cal">' + vitaminas_DATA[index].cal + '%cal' + '</p>' +
+                '</div>' +
+                '<div class="contenido_fruta">' + 
+                    '<img class="imagen_fruta_info" src="'+ vitaminas_DATA[index].alimentos_imagenes + '">' +
+                    '<img class="imagen_estadistica_info" src="'+ vitaminas_DATA[index].estadistica + '">' +
+                    '<img class="porcentaje_info" src="'+ vitaminas_DATA[index].porcentaje + '">' +
+                '</div>'
+            ;
+            contenedor.classList.add("contenedor_info");
+            info_alimentos.appendChild(contenedor);
+        });
+    }
+    agregar_info_alimentos();
 }
-pagina_lipidos();
+pagina_proteina();
