7e443afe8bd24f25f9064b4c6ecbcfce2ffb75a0
Steve Gomez Boror <stevegomitas3@gmail.com>
2022-10-05 21:53:40 -0600

    Se mejoro el aspecto en computadora.

--- js/componentes/estadistica.js
+++ js/componentes/estadistica.js
@@ -5,54 +5,31 @@ export function estadistica(){
     function insertar_plato(){
         let plato = 
             '<div class="seccion_estadistica">' + 
-                '<div class="seccion_plato">' + 
-                    '<div class="circulo_1">' + 
-                        
-                    '</div>' +
-                    '<div class="circulo_2">' + 
-                        
-                    '</div>' +
-                    '<div class="circulo_3">' + 
-                        
-                    '</div>' +
-                    '<div class="circulo_4">' + 
-                        
-                    '</div>' +
-                    '<div class="circulo_5">' + 
-                        
-                    '</div>' +
-                    '<div class="circulo_6">' + 
-                        
-                    '</div>' +
-                    '<img src="https://github.com/NottStevE/Imagenes/blob/main/Proyecto%20aps/Component%2010%20(3).png?raw=true">' +
-                    '<h2 class="porcentaje_1">' + '25%' + '</h2>' +
-                    '<h2 class="porcentaje_2">' + '50%' + '</h2>' +
-                    '<h2 class="porcentaje_3">' + '25%' + '</h2>' +
-                    '<p class="texto_plato">' + 'La principal característica del plato saludable es que define las porciones según cada grupo de alimentos y reconoce aquellos que, aunque están fuera de él, debes consumir a diario. Un plato saludable debe tener los siguientes grupos alimenticios esenciales: cereales integrales, proteínas, frutas y verduras. ' + '</p>' +
-                '</div>' +
+            '<img src="https://github.com/NottStevE/Imagenes/blob/main/Proyecto%20aps/plato%20compu.png?raw=true" class="plato_img">' +
+            '<p class="texto_plato">' + 'La principal característica del plato saludable es que define las porciones según cada grupo de alimentos y reconoce aquellos que, aunque están fuera de él, debes consumir a diario. Un plato saludable debe tener los siguientes grupos alimenticios esenciales: cereales integrales, proteínas, frutas y verduras.' + '</p>' +
                 '<div class="estadistica">' +
                     '<h2>' + 'Estadística' + '</h2>' +
-                    '<p>' + 'Esta gráfica hace referencia al consumo de diferentes grupos de alimentos que deberías consumir distribuidos a lo largo del día.' + '</p>' +
-                    '<p>' + 'Es una guía para que realices de forma variada y armónica una alimentación completa.' + '</p>' +
+                    '<p class="texto_estadistica_1">' + 'Esta gráfica hace referencia al consumo de diferentes grupos de alimentos que deberías consumir distribuidos a lo largo del día.' + '</p>' +
+                    '<p class="texto_estadistica_2">' + 'Es una guía para que realices de forma variada y armónica una alimentación completa.' + '</p>' +
                     '<img src="https://github.com/NottStevE/Imagenes/blob/main/Proyecto%20aps/Grafico%20de%20pastel%20estadistica.png?raw=true" class="grafico_pastel">' +
                     '<div class="porcentajes_estadistica">' +
                         '<div class="caja_1_estadistica">' +
                             '<div class="circulo_naranja_estadistica">' +
                                 
                             '</div>' +
-                            '<p>' + '50% Verduras Hortalizas' + '</p>' +
+                            '<p class="porcentaje_1_estadistica">' + '50% Verduras Hortalizas' + '</p>' +
                         '</div>' +
                         '<div class="caja_1_estadistica">' +
                             '<div class="circulo_verde_estadistica">' +
                                     
                             '</div>' +
-                            '<p>' + '25% Proteínas' + '</p>' +
+                            '<p class="porcentaje_2_estadistica">' + '25% Proteínas' + '</p>' +
                         '</div>' +
                         '<div class="caja_1_estadistica">' +
                             '<div class="circulo_rojo_estadistica">' +
                                     
                             '</div>' +
-                            '<p>' + ' 25% Hidratos de Carbono' + '</p>' +
+                            '<p class="porcentaje_3_estadistica">' + ' 25% Hidratos de Carbono' + '</p>' +
                         '</div>' +
                     '</div>' +
                     '<div class="seccion_objetos_estadistica">' + 
