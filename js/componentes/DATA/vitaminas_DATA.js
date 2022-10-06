7d277dc911c884f2f2b6ecb4139e2cd8cc86b2c8
Marlon Martinez <totoreto3martinez@gmail.com>
2022-10-05 21:37:14 -0600

    actualize la base de datos de vitaminas

--- js/componentes/DATA/vitaminas_DATA.js
+++ js/componentes/DATA/vitaminas_DATA.js
@@ -1,42 +1,82 @@
 export let vitaminas_DATA = [
     {
         nombre: 'HUEVOS',
-        alimentos_imagenes: 'https://github.com/NottStevE/Imagenes/blob/main/Proyecto%20aps/VITAMINAS/Huevos%20(1).png?raw=true'
+        alimentos_imagenes: 'https://github.com/NottStevE/Imagenes/blob/main/Proyecto%20aps/VITAMINAS/Huevos%20(1).png?raw=true',
+        estadistica: 'https://github.com/marlon161/img/blob/main/Component%20118.png?raw=true',
+        porcentaje: 'https://github.com/marlon161/img/blob/main/Component%2018.png?raw=true',
+        kcal: '141',
+        cal: '155'
     },
     {
         nombre: 'LECHE',
-        alimentos_imagenes: 'https://github.com/NottStevE/Imagenes/blob/main/Proyecto%20aps/VITAMINAS/Leche.png?raw=true'
+        alimentos_imagenes: 'https://github.com/NottStevE/Imagenes/blob/main/Proyecto%20aps/VITAMINAS/Leche.png?raw=true',
+        estadistica: 'https://github.com/marlon161/img/blob/main/Component%20121.png?raw=true',
+        porcentaje: 'https://github.com/marlon161/img/blob/main/Component%2018.png?raw=true',
+        kcal: '141',
+        cal: '155'
     },
     {
         nombre: 'MELÓN',
-        alimentos_imagenes: 'https://github.com/NottStevE/Imagenes/blob/main/Proyecto%20aps/VITAMINAS/Mel%C3%B3n.png?raw=true'
+        alimentos_imagenes: 'https://github.com/NottStevE/Imagenes/blob/main/Proyecto%20aps/VITAMINAS/Mel%C3%B3n.png?raw=true',
+        estadistica: 'https://github.com/marlon161/img/blob/main/Component%20123.png?raw=true',
+        porcentaje: 'https://github.com/marlon161/img/blob/main/Component%2018.png?raw=true',
+        kcal: '0.881',
+        cal: '881'
     },
     {
         nombre: 'YOGURT',
-        alimentos_imagenes: 'https://github.com/NottStevE/Imagenes/blob/main/Proyecto%20aps/VITAMINAS/Yogurt.png?raw=true'
+        alimentos_imagenes: 'https://github.com/NottStevE/Imagenes/blob/main/Proyecto%20aps/VITAMINAS/Yogurt.png?raw=true',
+        estadistica: 'https://github.com/marlon161/img/blob/main/Component%20122.png?raw=true',
+        porcentaje: 'https://github.com/marlon161/img/blob/main/Component%2018.png?raw=true',
+        kcal: '22',
+        cal: '20'
     },
     {
         nombre: 'HÍGADO',
-        alimentos_imagenes: 'https://github.com/NottStevE/Imagenes/blob/main/Proyecto%20aps/VITAMINAS/H%C3%ADgado.png?raw=true'
+        alimentos_imagenes: 'https://github.com/NottStevE/Imagenes/blob/main/Proyecto%20aps/VITAMINAS/H%C3%ADgado.png?raw=true',
+        estadistica: 'https://github.com/marlon161/img/blob/main/Component%20120.png?raw=true',
+        porcentaje: 'https://github.com/marlon161/img/blob/main/Component%2018.png?raw=true',
+        kcal: '165',
+        cal: '130'
     },
     {
         nombre: 'ESPINACA',
-        alimentos_imagenes: 'https://github.com/NottStevE/Imagenes/blob/main/Proyecto%20aps/VITAMINAS/Espinaca.png?raw=true'
+        alimentos_imagenes: 'https://github.com/NottStevE/Imagenes/blob/main/Proyecto%20aps/VITAMINAS/Espinaca.png?raw=true',
+        estadistica: 'https://github.com/marlon161/img/blob/main/Component%20119.png?raw=true',
+        porcentaje: 'https://github.com/marlon161/img/blob/main/Component%2018.png?raw=true',
+        kcal: '22',
+        cal: '20'
     },
     {
         nombre: 'ZANAHORIA',
-        alimentos_imagenes: 'https://github.com/NottStevE/Imagenes/blob/main/Proyecto%20aps/VITAMINAS/Zanahoria.png?raw=true'
+        alimentos_imagenes: 'https://github.com/NottStevE/Imagenes/blob/main/Proyecto%20aps/VITAMINAS/Zanahoria.png?raw=true',
+        estadistica: 'https://github.com/marlon161/img/blob/main/Component%20125.png?raw=true',
+        porcentaje: 'https://github.com/marlon161/img/blob/main/Component%2018.png?raw=true',
+        kcal: '22',
+        cal: '20'
     },
     {
         nombre: 'GUISANTES',
-        alimentos_imagenes: 'https://github.com/NottStevE/Imagenes/blob/main/Proyecto%20aps/VITAMINAS/Guisantes.png?raw=true'
+        alimentos_imagenes: 'https://github.com/NottStevE/Imagenes/blob/main/Proyecto%20aps/VITAMINAS/Guisantes.png?raw=true',
+        estadistica: 'https://github.com/marlon161/img/blob/main/Component%20127.png?raw=true',
+        porcentaje: 'https://github.com/marlon161/img/blob/main/Component%2018.png?raw=true',
+        kcal: '165',
+        cal: '130'
     },
     {
         nombre: 'REMOLACHA',
-        alimentos_imagenes: 'https://github.com/NottStevE/Imagenes/blob/main/Proyecto%20aps/VITAMINAS/Remolacha.png?raw=true' 
+        alimentos_imagenes: 'https://github.com/NottStevE/Imagenes/blob/main/Proyecto%20aps/VITAMINAS/Remolacha.png?raw=true' ,
+        estadistica: 'https://github.com/marlon161/img/blob/main/Component%20126.png?raw=true',
+        porcentaje: 'https://github.com/marlon161/img/blob/main/Component%2018.png?raw=true',
+        kcal: '165',
+        cal: '130'
     },
     {
         nombre: 'ALMENDRAS',
-        alimentos_imagenes: 'https://github.com/NottStevE/Imagenes/blob/main/Proyecto%20aps/VITAMINAS/Almendras.png?raw=true'
+        alimentos_imagenes: 'https://github.com/NottStevE/Imagenes/blob/main/Proyecto%20aps/VITAMINAS/Almendras.png?raw=true',
+        estadistica: 'https://github.com/marlon161/img/blob/main/Component%20124.png?raw=true',
+        porcentaje: 'https://github.com/marlon161/img/blob/main/Component%2018.png?raw=true',
+        kcal: '141',
+        cal: '155'
     }
 ]
